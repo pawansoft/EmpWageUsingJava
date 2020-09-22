90a68afade7f7843e5f6447c91c37da4a2741401
Pawan Kumar <pk.soft29@gmail.com>
2020-09-22 11:27:00 +0530

    Added : Part Time functionality added

--- EmpWage.java
+++ EmpWage.java
@@ -5,15 +5,22 @@ import java.util.Random;
 public class EmpWage {
     public static void main(String[] args) {
         Random att=new Random();
-        int max=2;
+        int max=3;
+        int half_time=4;
         int wage_per_hour=20;
         int hour_in_day=8;
         int present=1;
         int check=att.nextInt(max);
+        int totalWage;
         if(check == present)
         {
-            int totalWage=wage_per_hour*hour_in_day;
+            totalWage=wage_per_hour*hour_in_day;
             System.out.println("total Wage for Day : " + totalWage);
+        }
+        else if (check == 2)
+        {
+            totalWage=wage_per_hour*half_time;
+            System.out.println("PartTime Wage : " + totalWage);
         }
         else
         {
