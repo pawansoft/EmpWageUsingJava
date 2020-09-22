aebffe763d1a8691e620a4a35a8248714362f150
Pawan Kumar <pk.soft29@gmail.com>
2020-09-22 11:17:39 +0530

    Added : Calculate Daily Wage functionality Added

--- EmpWage.java
+++ EmpWage.java
@@ -6,11 +6,14 @@ public class EmpWage {
     public static void main(String[] args) {
         Random att=new Random();
         int max=2;
+        int wage_per_hour=20;
+        int hour_in_day=8;
         int present=1;
         int check=att.nextInt(max);
         if(check == present)
         {
-            System.out.println("Employee is present");
+            int totalWage=wage_per_hour*hour_in_day;
+            System.out.println("total Wage for Day : " + totalWage);
         }
         else
         {
