45b660018d8cc2bb0fbe40d56cf1cc010b4fe29c
Pawan Kumar <pk.soft29@gmail.com>
2020-09-22 11:56:55 +0530

    Added : Calculate Wage for a month functionality added

--- EmpWage.java
+++ EmpWage.java
@@ -10,22 +10,31 @@ public class EmpWage {
         int wage_per_hour=20;
         int hour_in_day=8;
         int present=1;
-        int check=att.nextInt(max);
-        int totalWage;
-        switch (check)
+        int totalWage=0;
+        int total_day=0;
+        int total_hour=0;
+        int total_working_day=20;
+        while (total_day < 20)
         {
-            case 1:
-                totalWage=wage_per_hour*hour_in_day;
-                System.out.println("total Wage for Day : " + totalWage);
-                break;
-            case 2:
-                totalWage=wage_per_hour*half_time;
-                System.out.println("PartTime Wage : " + totalWage);
-                break;
-            default:
-                System.out.println("Employee is absent");
-                break;
+            int check=att.nextInt(max);
+            switch (check)
+            {
+                case 1:
+                    totalWage=totalWage+wage_per_hour*hour_in_day;
+                    total_day=total_day+1;
+                    break;
+                case 2:
+                    totalWage=totalWage+wage_per_hour*half_time;
+                    total_hour=total_hour+4;
+                    break;
+                default:
+                    
+                    break;
+            }
         }
+        System.out.println("Total Wage for month : "+totalWage);
+        System.out.println("total Days : " +total_day);
+        System.out.println("total hour : "+total_hour);
 
     }
 
