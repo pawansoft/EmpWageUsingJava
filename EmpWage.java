9b833de6a031bae6d0d9cd0487600919338885aa
Pawan Kumar <pk.soft29@gmail.com>
2020-09-22 11:08:50 +0530

    Added : check employee is present or absent functionality

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,21 @@
-class EmpWage
-{
-	public static void main(String []arg)
-	{
-		System.out.println("Welcome to master branch of employee wage");
-	}
+package com.EmpWage;
+
+import java.util.Random;
+
+public class EmpWage {
+    public static void main(String[] args) {
+        Random att=new Random();
+        int max=2;
+        int present=1;
+        int check=att.nextInt(max);
+        if(check == present)
+        {
+            System.out.println("Employee is present");
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+        }
+    }
+
 }
